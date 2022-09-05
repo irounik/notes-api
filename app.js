9c907f9a4a2bd06acc477a1f0f4adcb591e5680c
Rounik <rounik1020@gmail.com>
2022-09-05 15:16:30 +0530

    Adde auth routes

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose');
+const db = require('./db/config');
+const authRoutes = require('./routes/auth');
 const notesRoutes = require('./routes/notes');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/auth', authRoutes);
 app.use('/notes', notesRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 const initApp = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_DB_URL, { dbName: 'notes' });
+    await db.initConnection();
     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
   } catch (err) {
     console.error(err);
