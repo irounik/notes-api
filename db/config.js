30f12a3bfd13396527610d09cb247f59f2f3d967
Rounik <rounik1020@gmail.com>
2022-09-28 23:04:12 +0530

    Added DB_NAME in config

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 exports.initConnection = async () => {
-  await mongoose.connect(process.env.MONGO_DB_URL, { dbName: 'notes' });
+  await mongoose.connect(process.env.MONGO_DB_URL, { dbName: process.env.DB_NAME });
 };
