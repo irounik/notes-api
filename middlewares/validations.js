ee375077f2cd2691843804b28e1afdd826d526da
Rounik <rounik1020@gmail.com>
2022-09-28 23:05:34 +0530

    Refactored validation logic

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,25 +1,27 @@
 const { body, validationResult } = require('express-validator');
 const { badRequest } = require('../utils/response');
 
+const validateEmail = body('email', 'Invalid email').exists().isEmail();
+
+const validatePassword = body('password', 'Invalid password, it should be at least 6 digit long')
+  .exists()
+  .isLength({ min: 6 });
+
+const validateUserName = body('name', 'Invalid username')
+  .exists()
+  .matches(/^[a-zA-Z ]+$/);
+
+const checkErrors = (message) => (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) next();
+  else badRequest(res, message, errors.errors);
+};
+
 exports.validateSignUp = [
-  body('name', 'Invalid username')
-    .exists()
-    .matches(/^[a-zA-Z ]+$/),
-  body('email', 'Invalid email').exists().isEmail(),
-  body('password', 'Invalid password, it should be atlease 6 digit long').exists().isLength({ min: 6 }),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (errors.isEmpty()) next();
-    else badRequest(res, `Invalid inputs for creating user`, errors);
-  },
+  validateUserName,
+  validateEmail,
+  validatePassword,
+  checkErrors('Invalid inputs for creating user'),
 ];
 
-exports.validateLogin = [
-  body('email', 'Invalid email').exists().isEmail(),
-  body('password', 'Invalid password, it should be atlease 6 digit long').exists().isLength({ min: 6 }),
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (errors.isEmpty()) next();
-    else badRequest(res, `Invalid inputs for creating user`, errors);
-  },
-];
+exports.validateLogin = [validateEmail, validatePassword, checkErrors('Invalid inputs for logging in')];
