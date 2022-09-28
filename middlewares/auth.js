f29711cadce6bacd2a0188de080bb0dae9b537b6
Rounik <rounik1020@gmail.com>
2022-09-28 23:04:37 +0530

    Created Authorization constant

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
 const responses = require('../utils/response');
+const contants = require('../utils/constants');
 
 module.exports = (req, res, next) => {
-  const token = req.header('token');
+  const token = req.header(contants.Authorization).split(' ')[1];
   if (!token) return responses.unauthorized(res, 'User is not authorized');
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
